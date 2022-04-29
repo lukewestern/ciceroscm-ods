6fc204db15d493f4637d73ec908c776bf6ebb3bd
ragnhibs <r.b.skeie@cicero.oslo.no>
2022-04-29 09:44:40 +0200

    update script

--- scripts/run_scm.py
+++ scripts/run_scm.py
@@ -14,11 +14,13 @@ data_dir = os.path.join(os.path.dirname(__file__), "../", "tests", "test-data")
 cscm = CICEROSCM()
 
 outdir = os.path.join(os.getcwd(), "output_test")
+prefix = "test1"
 
 cscm._run(
     {
         "gaspamfile": os.path.join(data_dir, "gases_v1RCMIP.txt"),
         "output_folder": outdir,
+        "output_prefix": prefix,
         "nyend": 2100,
         "concentrations_file": os.path.join(data_dir, "ssp245_conc_RCMIP.txt"),
         "emissions_file": os.path.join(data_dir, "ssp245_em_RCMIP.txt"),
